b278df12f6f3d2e0bf046aad682a9ed222887301
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-07 20:57:23 +1000

    Beginning User views - by Andrea during liveshare session

--- mentorship/mentorship/urls.py
+++ mentorship/mentorship/urls.py
@@ -22,6 +22,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
