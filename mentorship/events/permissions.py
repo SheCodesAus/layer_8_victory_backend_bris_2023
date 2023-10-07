725c8958242aa956275fc491801e423b2b2bf144
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-07 19:21:24 +1000

    Ensuring only staff can create events. We now have a custom staff permission as well as a super admin permission.

--- mentorship/events/permissions.py
+++ mentorship/events/permissions.py
@@ -1,5 +1,20 @@
 from rest_framework import permissions
 
+class CustomIsAdmin(permissions.BasePermission):
+  def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_staff
+        )
+  
 class IsSuperAdmin(permissions.BasePermission):
-  def has_object_permission (self,request,view,obj):
-    return bool(request.user.is_superuser and request.user.is_staff)
+  def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_superuser and 
+            request.user.is_staff
+        )
--- mentorship/events/views.py
+++ mentorship/events/views.py
@@ -1,23 +1,24 @@
 from rest_framework import status, permissions
 from rest_framework.views import APIView
-from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from .models import Event, EventMentors
 from .serializers import EventSerializer, EventMentorsSerializer
-from .permissions import IsSuperAdmin
+from .permissions import IsSuperAdmin, CustomIsAdmin
 
 
 class EventList(APIView):
-
+    permission_classes = [CustomIsAdmin]
     def get(self, request):
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
 
-    @permission_classes(IsSuperAdmin)
     def post(self, request):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
-            if request.user.is_staff and request.user.is_superuser:
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            serializer.save(modified_by=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
