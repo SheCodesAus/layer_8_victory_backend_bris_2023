6f0df8aa94c9e0c79c644959cac59a0b8dec5714
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-07 18:50:17 +1000

    removing accidental copy and paste (we're not using the ASCIIUsernameValidator)

--- mentorship/users/serializers.py
+++ mentorship/users/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.contrib.auth.validators import ASCIIUsernameValidator
 
 class CustomUserSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.id')
