7dbae75227909b371a330b2e157058263857f43c
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-09 14:06:27 +1000

    Adding created_by field for events & event mentors. Also added comment querying best approach for end date validation.

--- mentorship/events/models.py
+++ mentorship/events/models.py
@@ -5,9 +5,16 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    # we should have some validation to check that end date is after start date.
+    # Would that be best achieved in front end or back end?
     location = models.CharField(max_length=200)
     is_published = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='event_created'
+    )
     last_modified = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(
         get_user_model(),
@@ -27,6 +34,11 @@ class EventMentors(models.Model):
         related_name='event_mentors'
     )
     date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='event_mentor_created'
+    )
     last_modified = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(
         get_user_model(),
