bc5645c21ad012f9ad1cc74aae617ad2fae385d2
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-16 18:17:01 +1000

    Correcting SECRET_KEY variable

--- mentorship/mentorship/settings.py
+++ mentorship/mentorship/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v2)e@u-_1zo-k(mh3$4f*ft&l*inebl)1q8s8velzcwv1pt%d$'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-v2)e@u-_1zo-k(mh3$4f*ft&l*inebl)1q8s8velzcwv1pt%d$'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get(
